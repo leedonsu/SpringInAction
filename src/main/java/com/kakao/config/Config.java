8efc6f7d79e833e4fbfd1beed63ad73a9b557915
leedonsu <donsu.lee@gmail.com>
2017-02-27 04:59:10 +0900

    Annotation Scope

--- src/main/java/com/kakao/config/Config.java
+++ src/main/java/com/kakao/config/Config.java
@@ -3,6 +3,7 @@ package com.kakao.config;
 import com.kakao.module.bean.domain.Book;
 import com.kakao.module.bean.domain.Phone;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
@@ -10,9 +11,11 @@ import org.springframework.context.annotation.Scope;
  * Created by Louis on 2017-02-27.
  */
 @Configuration
+@ComponentScan(basePackages = "com.kakao.module.bean")
 public class Config {
 
     // https://en.wikipedia.org/wiki/JSR_250
+    // JSR-330 metadata.
     @Scope("prototype")
     @Bean(initMethod = "init")
     public Book getBook(){
